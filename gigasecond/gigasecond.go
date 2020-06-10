5c1669afd3c5d02f9932e1a9250bf400d53bad6a
Michael-Bryant Choa <mbchoa@gmail.com>
2020-06-09 22:58:30 -0700

    docs: Update Gigasecond solution with comments

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,16 +1,11 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package gigasecond provides the date and time plus 1 Gigasecond (1e9 seconds) in the future
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
 const GIGASECOND = time.Duration(1e9) * time.Second
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond adds a single gigasecond to the input time and returns it
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(GIGASECOND)
 }
